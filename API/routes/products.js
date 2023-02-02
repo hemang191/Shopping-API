314c08cf068ea76a240c78e91372906b232986c7
Hemang <hemangsharma1001@gmail.com>
2023-02-02 10:55:02 +0530

    second step

--- API/routes/products.js
+++ API/routes/products.js
@@ -1,7 +1,6 @@
 const express = require('express') ; 
 const router = express.Router() ; 
 
-
 router.get('/' , (req , res , next)=>
 {
     res.status(200).json(
@@ -13,9 +12,14 @@ router.get('/' , (req , res , next)=>
 
 router.post('/'  , (req, res , next)=>
 {
+    const product = {
+        name : req.body.name , 
+        price : req.body.price 
+    };
     res.status(201).json(
         {
-            message: 'Handling POST request to products'
+            message: 'Handling POST request to products',
+            createdProduct : product
         }
     );
 })
--- app.js
+++ app.js
@@ -1,12 +1,67 @@
 const express = require('express') ; 
 const app = express() ; 
+const bodyParser = require('body-parser') ; 
+const mongoose = require('mongoose');
+
+require('dotenv').config() ; 
+
+const db = process.env.dbLink ;
+
+// Database Connection
+mongoose.set('strictQuery' , true) 
+mongoose.connect(db , {useNewUrlParser : true})
+.then(()=> console.log('MongoDB connected'))
+.catch(err=> console.log(err)) ; 
+
+
+
 
 
 const productRoutes = require('./API/routes/products') ; 
 const orderRoutes = require('./API/routes/orders') ;
 
+
+app.use(bodyParser.urlencoded({extended: false})) ; 
+app.use(bodyParser.json()) ; 
+
+// Handling cors error only occur on browser not on POSTMAN 
+app.use((req , res , next)=>
+{
+    res.header("Acess-Control-Allow-Origin" , "*") ; 
+    res.header(
+        "Access-Control-Allow-Headers" , 
+        "Origin , X-Requested-With , Content-Type , Accept , Authorization"
+    );
+    if(req.method === 'OPTIONS')
+    {
+        res.header('Access-Control-Allow-Methods' , 'PUT , POST , PATCH ,DELETE');
+        return res.status(200).json({}) ; 
+    }
+
+    next() ; 
+})
 app.use('/products' , productRoutes) ; 
 app.use('/orders' , orderRoutes) ; 
 
+// here this middleware handles the request for routes not present
+app.use((req , res , next)=>
+{
+    const error = new Error('Not found') ; 
+  
+    error.status(404) ; 
+    next(error) ;
+});
+
+
+// here this middleware handles the request for the db errors and others one 
+app.use((error , req , res , next) =>
+{
+    res.status(error.status || 500);
+    res.json({
+        error:{
+            message : error.message 
+        }
+    });
+});
 
 module.exports = app ; 
